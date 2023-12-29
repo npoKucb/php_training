cbcf4dd1d53ea7956494e770abf378a607717109
npoKucb <71996346+npoKucb@users.noreply.github.com>
2023-12-29 13:50:50 +0200

    Update basics.php

--- samples/basics.php
+++ samples/basics.php
@@ -1,1 +1,13 @@
 <?php
+
+$test1 = 0;
+var_dump($test1);
+
+$_test_2 = 2;
+var_dump($_test_2);
+
+$testVariable = 'ololo';
+var_dump($testVariable);
+
+$testVariable = 'alala';
+var_dump($testVariable);
